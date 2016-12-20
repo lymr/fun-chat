bf90dd9f200f34fbc33e372294eb42046e49f01f
mor_l <levy.mor@gmail.com>
2016-12-20 23:50:21 +0200

    * fixed PredicateMatcher

--- fun-chat-server/src/test/scala/base/PredicateMatcher.scala
+++ fun-chat-server/src/test/scala/base/PredicateMatcher.scala
@@ -2,11 +2,14 @@ package base
 
 import org.mockito.ArgumentMatcher
 
+import scala.util.Try
+
 case class PredicateMatcher[A](predicate: (A) => Boolean) extends ArgumentMatcher[A] {
   override def matches(argument: scala.Any): Boolean = {
-    argument match {
-      case (arg: A) => predicate(arg)
-      case _        => false
-    }
+    Try {
+      predicate(argument.asInstanceOf[A])
+    }.recover {
+      case _: Exception => false
+    }.getOrElse(false)
   }
 }
