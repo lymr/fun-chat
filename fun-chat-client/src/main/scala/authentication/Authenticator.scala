91500981993f3457db749d502caa26b11993e46d
mor_l <levy.mor@gmail.com>
2017-03-04 21:22:03 -0800

    * fix authenticator

--- fun-chat-client/src/main/scala/authentication/Authenticator.scala
+++ fun-chat-client/src/main/scala/authentication/Authenticator.scala
@@ -1,12 +1,12 @@
 package authentication
 
 import akka.actor.{Actor, ActorLogging, Props}
+import akka.pattern.pipe
 import authentication.entities.{AuthToken, _}
 import rest.client.RestClient
 import rest.client.entities.ExecutionResultCode
 
 import scala.concurrent.ExecutionContext
-import scala.util.{Failure, Success}
 
 class Authenticator(restClient: RestClient) extends Actor with ActorLogging {
 
@@ -14,23 +14,32 @@ class Authenticator(restClient: RestClient) extends Actor with ActorLogging {
 
   override def receive: Receive = {
     case SignIn(user, password) =>
-      restClient.signIn(user, password).onComplete {
-        case Success(token: AuthToken) => AuthTokenStore.updateToken(token); sender() ! Authenticated
-        case Failure(error)            => sender() ! AuthFailure(error)
-      }
+      restClient.signIn(user, password)
+        .recover {
+          case ex: Throwable => AuthFailure(ex)
+        }
+        .map {
+          case (token: AuthToken) => AuthTokenStore.updateToken(token); Authenticated
+        } pipeTo sender()
 
     case SignUp(user, password) =>
-      restClient.signUp(user, password).onComplete {
-        case Success(token: AuthToken) => AuthTokenStore.updateToken(token); sender ! Authenticated
-        case Failure(error)            => sender ! AuthFailure(error)
-      }
+      restClient.signUp(user, password)
+        .recover {
+          case ex: Throwable => AuthFailure(ex)
+        }
+        .map {
+          case (token: AuthToken) => AuthTokenStore.updateToken(token); Authenticated
+        } pipeTo sender()
 
     case SignOut =>
-      restClient.signOut().onComplete {
-        case Success(ExecutionResultCode.OK) => AuthTokenStore.clear(); sender ! Disconnected
-        case Success(_)                      => sender ! AuthFailure(new RuntimeException("Operation failed with unknown error"))
-        case Failure(error)                  => sender ! AuthFailure(error)
-      }
+      restClient.signOut()
+        .recover {
+          case ex: Throwable => AuthFailure(ex)
+        }
+        .map {
+          case ExecutionResultCode.OK => AuthTokenStore.clear(); Disconnected
+          case _                      => AuthFailure(new RuntimeException("Operation failed with unknown error"))
+        } pipeTo sender()
   }
 }
 
