d4b438e6941fc1935270cab905f7e3073e196809
mor_l <levy.mor@gmail.com>
2017-07-04 22:17:14 -0700

    http route refactoring

--- fun-chat-server/src/main/scala/restapi/http/routes/HttpRouter.scala
+++ fun-chat-server/src/main/scala/restapi/http/routes/HttpRouter.scala
@@ -4,26 +4,27 @@ import akka.actor.ActorRef
 import akka.http.scaladsl.server.{Directives, Route}
 import core.authentication.AuthenticationService
 import core.db.DatabaseContext
-import core.db.clients.ConnectedClientsStore
 import core.entities.{AuthToken, AuthTokenContext, User}
 import restapi.http.JsonSupport
 import restapi.http.routes.support.CorsSupport
 import utils.Configuration
+import websocket.WebSocketHandler
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class HttpRouter(dbc: DatabaseContext,
                  authService: AuthenticationService,
-                 connectedClients: ConnectedClientsStore,
+                 webSocketHandler: WebSocketHandler,
                  messagesRouter: ActorRef,
-                 configuration: Configuration)(implicit ec: ExecutionContext)
+                 connectedClientsStore: ActorRef,
+                 configuration: Configuration)(implicit apiDispatcher: ExecutionContext)
     extends Directives with JsonSupport with CorsSupport {
 
   private implicit val ac = new ApiContext(authService.authorize, dbc.usersDao.findUserByName)
 
-  private val userRoute     = new UsersRoute(dbc.usersDao)
-  private val authRoute     = new AuthenticationRoute(authService)
-  private val messagesRoute = new MessagesRoute(messagesRouter, configuration.messageTimeout)
+  private val userRoute = new UsersRoute(dbc.usersDao)
+  private val authRoute = new AuthenticationRoute(authService)
+  private val messagesRoute = new MessagesRoute(messagesRouter, webSocketHandler, dbc.usersDao, configuration.messageTimeout)
 
   val routes: Route = pathPrefix("v1") {
     AccessControlCheck {
--- fun-chat-server/src/main/scala/restapi/http/routes/MessagesRoute.scala
+++ fun-chat-server/src/main/scala/restapi/http/routes/MessagesRoute.scala
@@ -1,40 +1,60 @@
 package restapi.http.routes
 
 import akka.actor.ActorRef
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.{Directives, Route}
 import akka.pattern.ask
 import akka.util.Timeout
-import api.entities.MessageEntity
-import messages.entities.{ForwardRawMessage, MessageProcessingResult, ProcessingDone, ProcessingFailure}
+import api.entities.{MessageEntity, MessageProcessingCodes, MessageProcessingResponse}
+import core.db.users.UsersDao
+import core.entities.AuthTokenContext
+import messages.MessageProcessor.ForwardRawMessage
 import restapi.http.JsonSupport
 import restapi.http.routes.support.SecuredAccessSupport
+import websocket.WebSocketHandler
 
-import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
 
-class MessagesRoute(messagesRouter: ActorRef, processingTimeout: FiniteDuration)
-                   (implicit ec: ExecutionContext, ac: ApiContext)
+class MessagesRoute(messagesRouter: ActorRef,
+                    webSocketHandler: WebSocketHandler,
+                    usersDao: UsersDao,
+                    processingTimeout: FiniteDuration)(implicit ec: ExecutionContext, ac: ApiContext)
     extends Directives with SecuredAccessSupport with JsonSupport {
 
   implicit val timeout = Timeout(processingTimeout)
 
-  val route: Route = pathPrefix("messages") {
-    securedAccess { ctx =>
-      pathEndOrSingleSlash {
-        post {
-          decodeRequest {
-            entity(as[MessageEntity]) { message =>
-              val messageFuture = { messagesRouter ? ForwardRawMessage(message, ctx) }.mapTo[MessageProcessingResult]
-              onSuccess(messageFuture) {
-                case ProcessingDone           => complete(StatusCodes.OK)
-                case ProcessingFailure(cause) => complete(StatusCodes.BadRequest, cause)
+  val route: Route =
+    pathPrefix("messages") {
+      securedAccess { ctx =>
+        pathEndOrSingleSlash {
+          post {
+            decodeRequest {
+              entity(as[MessageEntity]) { message =>
+                complete(handleMessage(message, ctx))
               }
             }
+          }
+        } ~ path("ws") {
+          get {
+            handleWebSocketMessages(webSocketHandler.clientEndpoint(ctx))
           }
         }
       }
     }
-  }
 
+  private def handleMessage(message: MessageEntity, ctx: AuthTokenContext): Future[MessageProcessingResponse] = {
+    Future(usersDao.findUserByID(ctx.userId))
+      .flatMap {
+        case Some(user) =>
+          (messagesRouter ? ForwardRawMessage(message, user)).mapTo[MessageProcessingResponse]
+
+        case None =>
+          Future.successful(
+            MessageProcessingResponse(MessageProcessingCodes.Error,
+                                      s"Something went wrong! user:= ${ctx.username} not found!"))
+      }
+      .recover {
+        case ex: Exception => MessageProcessingResponse(MessageProcessingCodes.Error, ex.getMessage)
+      }
+  }
 }
