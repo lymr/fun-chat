53f36667583b5df208c1c06ec08257eb212d6068
mor_l <levy.mor@gmail.com>
2017-07-06 23:33:45 -0700

    minor change

--- fun-chat-common/src/test/scala/tests/TestWordSpec.scala
+++ fun-chat-common/src/test/scala/tests/TestWordSpec.scala
@@ -1,8 +1,6 @@
 package tests
 
-import org.scalatest.{AsyncWordSpecLike, Matchers, WordSpecLike}
+import org.scalatest.{Matchers, WordSpecLike}
 import tests.support.MockitoSupport
 
 trait TestWordSpec extends WordSpecLike with Matchers with MockitoSupport
-
-trait AsyncTestWordSpec extends AsyncWordSpecLike with Matchers with MockitoSupport
